eb464fe51771d3a4701364d22c8eedfe3f3db964
rohan <rohandasari098@gmail.com>
2023-12-03 03:06:37 +0000

    clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,25 @@
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+import {useState, useEffect} from 'react'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(timerId)
+    }
+  }, [])
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time> {date.toLocalTimeString()} </Time>
+    </ClockContainer>
+  )
+}
+export default Clock
